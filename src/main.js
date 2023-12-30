024437b87dbe9583675968c84628e12b8b997d64
agung permana <144643400+gunggpermanaa@users.noreply.github.com>
2023-12-30 16:14:22 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,47 @@
+let search = document.querySelector(".search-box");
+let cart = document.querySelector(".cart");
+let user = document.querySelector(".user");
+
+document.querySelector("#search-icon").onclick = () => {
+  search.classList.toggle("active");
+  cart.classList.remove("active");
+  user.classList.remove("active");
+};
+
+document.querySelector("#cart-icon").onclick = () => {
+  cart.classList.toggle("active");
+  search.classList.remove("active");
+  user.classList.remove("active");
+};
+
+document.querySelector("#user-icon").onclick = () => {
+  user.classList.toggle("active");
+  search.classList.remove("active");
+  cart.classList.remove("active");
+};
+
+// Navbar Scrool
+let header = document.querySelector('header');
+
+window.addEventListener('scroll',() => {
+  header.classList.toggle('shadow',window.scrollY > 0);
+});
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  slidesPerView: 3,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
